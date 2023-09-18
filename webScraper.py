40446fd8cec5393a1589db1f1d246b7c0185d591
Nimantha Kasun <85643915+nimantha2000@users.noreply.github.com>
2023-09-18 10:53:36 +0530

    Update webScraper.py
    modify with search from multiple web site addition to local.

--- webScraper.py
+++ webScraper.py
@@ -15,35 +15,46 @@ with open(dataset_file, "r") as file:
 # Function to find the answer for a given question
 def find_answer_local(user_question, qa_dict):
     user_question = user_question.lower()  # Convert user's question to lowercase
-    return qa_dict.get(user_question, "I don't know the answer to that question.")
+    return qa_dict.get(user_question)
 
+def find_answer_internet(question):
+    websites = [
+        "https://en.wikipedia.org/wiki/Ceylon_Electricity_Board",
+        "https://www.ceb.lk/",  # Add your additional websites here
+        "https://anothersite.com"  # Add more websites if needed
+    ]
 
-def find_answer(question, website):
-    response = requests.get(website)
-    soup = bs4.BeautifulSoup(response.content, "html.parser")
+    user_question_lower = question.lower()
 
-    for paragraph in soup.find_all("p"):
-        if question in paragraph.text:
-            return paragraph.text
+    for website in websites:
+        response = requests.get(website)
+        soup = bs4.BeautifulSoup(response.content, "html.parser")
+
+        for paragraph in soup.find_all("p"):
+            if user_question_lower in paragraph.text.lower():
+                return paragraph.text
 
     return None
 
 def main():
-    question = input("What is your question? ")
-    website = "https://en.wikipedia.org/wiki/Ceylon_Electricity_Board"
+    question = input("What is your question?")
 
+    # First, try to find an answer locally
+    local_answer = find_answer_local(question, qa_dict)
 
-    answer = find_answer(question, website)
-
-    if answer is not None:
-        print(answer)
-    elif answer is None:
-        answer = find_answer_local(question , qa_dict)
-        print(answer)
+    # If an answer is found locally, print it
+    if local_answer:
+        print(local_answer)
     else:
-        print("I couldn't find an answer to your question.")
+        # If no answer is found locally, search on the internet
+        internet_answer = find_answer_internet(question)
 
-
+        # If an answer is found on the internet, print it
+        if internet_answer:
+            print(internet_answer)
+        else:
+            print("I couldn't find an answer to your question.")
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
